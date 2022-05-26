a5daaba0ba6749247e4fa081bcc168f4f41316e1
kirikooooooooo <maaaa.horizon@gmail.com>
2022-05-26 18:30:15 +0900

    deviseに関するビューファイルの配置

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :name_lastname, presence: true
+  validates :name_firstname, presence: true
+  validates :name_kana_lastname, presence: true
+  validates :name_kana_firstname, presence: true
+  validates :birthdate, presence: true
 end
