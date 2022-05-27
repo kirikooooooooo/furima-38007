5d71cfc6486776815cbdb531dbe6648f49361f14
kirikooooooooo <maaaa.horizon@gmail.com>
2022-05-27 15:10:29 +0900

    トップページからのリンク・devise_parameter_sanitizer実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :name_lastname, presence: true
-  validates :name_firstname, presence: true
-  validates :name_kana_lastname, presence: true
-  validates :name_kana_firstname, presence: true
-  validates :birthdate, presence: true
+  #validates :nickname, presence: true
+  #validates :name_lastname, presence: true
+  #validates :name_firstname, presence: true
+  #validates :name_kana_lastname, presence: true
+  #validates :name_kana_firstname, presence: true
+  #validates :birthdate, presence: true
+
+  #PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  #validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めて設定してください'
+
 end
