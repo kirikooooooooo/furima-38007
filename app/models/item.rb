69f11263cf17203ee3e998143d6175dbce7eaf8a
kirikooooooooo <maaaa.horizon@gmail.com>
2022-05-30 17:43:05 +0900

    バリデーション設定・カテゴリー表示実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
 
+  validates :name, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: "can't be blank"}
+  validates :user, presence: true
+
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
