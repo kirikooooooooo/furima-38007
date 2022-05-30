d0337dfa4e95a4ca0d8bccf541cf94fef600677b
kirikooooooooo <maaaa.horizon@gmail.com>
2022-05-30 15:20:41 +0900

    アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :order
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 end
