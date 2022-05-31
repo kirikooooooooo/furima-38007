76bd69e3d7cc9a579ecf08d6c5c1fd8a9e03f664
kirikooooooooo <maaaa.horizon@gmail.com>
2022-05-31 16:13:31 +0900

    商品出品機能テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,15 @@ class Item < ApplicationRecord
   has_one :order
   has_one_attached :image
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :explanation, presence: true, length: { maximum: 1000 }
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: "¥300~¥9,999,999の間で設定してください"}
-
+  validates :image, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,37 +10,77 @@ RSpec.describe Item, type: :model do
       it 'すべてのカラムが存在すれば登録できる' do
         expect(@item).to be_valid
       end
-      it 'テキストのみで投稿できる' do
-      end
     end
     context '商品が出品できない場合' do
       it '商品画像が空では投稿できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it '商品名が空では投稿できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
       end
       it '商品名が41文字以上では投稿できない' do
+        @item.name = Faker::String.random(length: [41, 60])
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
       end
       it '商品の説明が空では投稿できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
       it '商品の説明が1001文字以上では投稿できない' do
+        @item.explanation = Faker::String.random(length: [1001, 1030])
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation is too long (maximum is 1000 characters)")
       end
       it 'カテゴリーが空では投稿できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態が空では投稿できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it '配送料の負担が空では投稿できない' do
+        @item.delivery_charge_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery charge can't be blank")
       end
       it '配送元の地域が空では投稿できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数が空では投稿できない' do
+        @item.days_to_ship_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship can't be blank")
       end
       it '価格が空では投稿できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
       end
       it '価格が299以下では投稿できない' do
+        @item.price = Faker::Number.between(from: 0, to: 299)
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price ¥300~¥9,999,999の間で設定してください")
       end
       it '価格が10000000以上では投稿できない' do
+        @item.price = Faker::Number.between(from: 10000000, to: 100000000)
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price ¥300~¥9,999,999の間で設定してください")
       end
       it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
