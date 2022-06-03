fcacbc93d028f1440664cc5d768cc0e87c938592
kirikooooooooo <maaaa.horizon@gmail.com>
2022-06-03 23:24:22 +0900

    微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,10 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    unless current_user.id == @item.user_id
-      redirect_to action: :index
-    end
-    if Order.where(item_id: @item.id).exists?
+    if (current_user.id != @item.user_id) || Order.where(item_id: @item.id).exists?
       redirect_to action: :index
     end
   end
