cf97d1dec2bef5bf7990406c4b21b4dea007b51a
kirikooooooooo <maaaa.horizon@gmail.com>
2022-06-07 14:46:04 +0900

    エラーメッセージの日本語化追加実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,11 @@ class Item < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 40 }
   validates :explanation, presence: true, length: { maximum: 1000 }
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :category_id, numericality: { other_than: 1 , message: "を選択してください"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "を選択してください"} 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "を選択してください"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "を選択してください"} 
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "を選択してください"} 
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 , message: "¥300~¥9,999,999の間で設定してください"}
   validates :image, presence: true
 
